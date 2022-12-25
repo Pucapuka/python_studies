4df688eccc20cd41c0324066927a90c9d4b54e0a
Paulo Lima <102117492+Pucapuka@users.noreply.github.com>
2022-12-25 18:58:03 -0300

    Create multiple Lines.java
    working on lines displaying.

---
+++ multiple Lines.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main (String[] args){
+    //multiple lines with a print in each line
+ 	  System.out.println("Hi, everybody!");
+    System.out.println("My name is Paulo.");
+    System.out.println("I´m learning Java. I´m loving it!");
+    
+    //working the commands print() and println()
+    
+    System.out.println("Hello World! ");
+    System.out.print("This ways of using print command: ");
+    System.out.println("will display the content on the same line.");
+    System.out.print("This one will be printed on the next line for the previous command was 'println'.");
+  }
+}
