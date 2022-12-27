a6c534f9926e2735fad4014bb2de797885a43411
Paulo Lima <102117492+Pucapuka@users.noreply.github.com>
2022-12-27 11:02:54 -0300

    Update Hangman.py
    impoved, but still needs some improvements!

--- Hangman.py
+++ Hangman.py
@@ -1,24 +1,34 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
 import random
 
 wordList = ("change", "merge", "foreign", "country")
 repeat = []
 chosenWord = random.choice(wordList)
 chances = 6
-
+booleankey = True
 
 while True:
+  if booleankey == False:
+      break
+  else:
     chosenLetter = input("Type a letter: ")
     if chosenLetter not in repeat:
         repeat.append(chosenLetter)
         if chosenLetter in chosenWord:
             print("You found one letter!")
-            False
+            booleankey = False
             for i in chosenWord:
                 if i in repeat:
                     print(i, end="")
                 else:
                     print("_", end="")
-                    True
+                    booleankey = True
             print()
         else:
             print("This letter isn't in the word!")
@@ -30,6 +40,9 @@ while True:
     if chances<=0:
         print("You got no more chances! Your man is dead!")
         break
+
+if booleankey == False:
+    print(f"Congratulations! You saved your man! Answer: {chosenWord}")
 print ("GAME OVER!")
     
     
