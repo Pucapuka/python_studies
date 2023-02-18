8fbe4ecc46839f9d528ce355da245c46ab36e103
Paulo Lima <102117492+Pucapuka@users.noreply.github.com>
2023-02-18 11:27:58 -0300

    Create DIO-Java-loops-Exercicio_1.java
    Exercício 1 da DIO, prof. Cami-la.

---
+++ DIO-Java-loops-Exercicio_1.java
@@ -0,0 +1,30 @@
+/*
+Faça um programa que leia conjuntos de dois valores,
+o primeiro representando o nome do aluno e o segundo representando a sua idade.
+(Pare o programa inserindo o valor 0 no campo nome)
+*/
+
+import java.util.Scanner;
+
+public class Main{
+
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		String nome;
+		int idade;
+		System.out.println("Dados do Aluno");
+		
+	while (true){
+		System.out.print("Nome: ");
+		nome = scan.next();
+		
+		if (nome.equals("0")) break;
+		
+		System.out.print("Idade: ");
+		idade = scan.nextInt();
+		
+		System.out.print("O Aluno " + nome + " tem " + idade + " anos de idade.\n");
+	    
+	}
+ }
+}
