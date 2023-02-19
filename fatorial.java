36668efba5c6ab3ac3d50a9c954d3fd0339835ed
Paulo Lima <102117492+Pucapuka@users.noreply.github.com>
2023-02-18 21:15:43 -0300

    Update fatorial.java
    Melhorei o código, colocando entrada de valores pela classe Scanner.

--- fatorial.java
+++ fatorial.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Main{
     public static int fatorial(int numero){
-        int fator, multiplo, i;
+	int fator, multiplo, i;
         fator = numero;
         multiplo = 1;
         for (i = 1; i < numero; i++){
@@ -11,6 +13,10 @@ public class Main{
     }
 	
 	public static void main (String[]args){
-	  System.out.println (fatorial(4));
+	    Scanner scan = new Scanner(System.in);
+	    int valor;
+	    System.out.print("Quer calcular o fatorial de qual número? ");  
+	    valor = scan.nextInt();
+	    System.out.println ("O fatorial de "+ valor + " é " + fatorial(valor) + ".");
 	}
   }
